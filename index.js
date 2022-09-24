99c4fc8128def68bd856916cc7e080ced94ebcb2
hungit1993qt <hungit1993qt@gmail.com>
2022-09-24 12:58:02 +0700

    update serverAPI

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { engine } = require ('express-handlebars');
 const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
@@ -10,7 +11,7 @@ const storeRoute = require("./routes/store");
 const partnerRoute = require("./routes/partner");
 const newsRoute = require("./routes/news");
 const managerRoute = require("./routes/manager");
-
+app.use(express.static(__dirname + '/'))
 dotenv.config();
 // connect database
 mongoose
@@ -29,12 +30,17 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(cors());
 app.use(morgan("common"));
 
+//Template engine
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
 //Routs
 app.use("/t52/client/", clientRoute);
 app.use("/t52/store/", storeRoute);
 app.use("/t52/partner/", partnerRoute);
 app.use("/t52/news/", newsRoute);
 app.use("/t52/manager/", managerRoute);
-app.use("/", (req, res) => {
-  res.send("Server đang chạy.....");
-});
