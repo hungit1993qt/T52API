142ebfc1afa0383d687229544772aadb181dd4dd
hungit1993qt <hungit1993qt@gmail.com>
2022-09-28 17:34:10 +0700

    fix

--- controllers/partnerController.js
+++ controllers/partnerController.js
@@ -4,7 +4,7 @@ const partnerController = {
     try {
       const newPartner = new Partner(req.body);
       if (req.file) {
-        news.img = "https://t52-loan-nodejs.herokuapp.com/" + req.file.path;
+        newPartner.img = "https://t52-loan-nodejs.herokuapp.com/" + req.file.path;
       }
       const savedPartner = await newPartner.save();
       res.status(200).json(savedPartner);
