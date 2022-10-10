a5a6665cd85a19c390abe16dd1a5371c3e20e3f0
hungit1993qt <hungit1993qt@gmail.com>
2022-10-10 10:24:16 +0700

    fix

--- index.js
+++ index.js
@@ -1,55 +1,12 @@
 const express = require("express");
-const { engine } = require("express-handlebars");
-const cors = require("cors");
+
 const app = express();
-const mongoose = require("mongoose");
-var bodyParser = require("body-parser");
-const morgan = require("morgan");
-const dotenv = require("dotenv");
-const cookieParser = require("cookie-parser");
-const clientRoute = require("./routes/client");
-const storeRoute = require("./routes/store");
-const partnerRoute = require("./routes/partner");
-const newsRoute = require("./routes/news");
-const mediasRoute = require("./routes/medias");
-const managerRoute = require("./routes/manager");
-const bannersRoute = require("./routes/banners");
-const verifyToken = require("./middleware/verifyToken");
+require("dotenv").config();
+app.use(express.json());
 
-app.use(express.static(__dirname + "/"));
-dotenv.config();
-// connect database
-
-mongoose
-  .connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("conect to mongo");
-    app.listen(process.env.PORT || 8000, () => {
-      console.log("Server running...");
-    });
-  });
-
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(cors());
-app.use(cookieParser());
-app.use(morgan("common"));
-
-//Template engine
-app.engine("handlebars", engine());
-app.set("view engine", "handlebars");
-app.set("views", "./views");
-app.get("/", (req, res) => {
-  res.render("home");
+const PORT = process.env.PORT || 8080;
+const routes = require("./routes/productRouter.js");
+app.use("/products", routes);
+app.listen(PORT, () => {
+  console.log("Server ís running");
 });
-
-//Routs
-app.use("/t52/client/", clientRoute);
-app.use("/t52/store/", storeRoute);
-app.use("/t52/partner/", partnerRoute);
-app.use("/t52/news/", newsRoute);
-app.use("/t52/medias/", mediasRoute);
-app.use("/t52/manager/", managerRoute);
-app.use("/t52/banners/", bannersRoute);
